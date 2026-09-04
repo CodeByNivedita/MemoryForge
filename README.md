a8e0deff8244152e1729071094a39739500c4db4
indra suthar <indrasuthar14@gmail.com>
2026-09-04 16:34:40 +0530

    chore: initial setup with Vite, React, TS, Tailwind

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# MemoryForge
+MemoryForge
+
+
+```
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+       hi
+      </section>
+    </>
+  )
+}
+
+export default App
